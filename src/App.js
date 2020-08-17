63753bc0bd50b5d94b6ad84a8b6dc5f9be175c87
Oksana Tkachenko <coatceatl@gmail.com>
2020-08-17 22:48:26 +0300

    added data for title, labels

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { LineChart } from './LineChart';
 function App() {
   return (
     <div className='container py-4'>
-      <div className='row'>
+      <div className='row justify-content-center align-items-center'>
         <div className='col-12 col-md-6'>
 
           <div className='card shadow rounded'>
--- src/LineChart.js
+++ src/LineChart.js
@@ -4,30 +4,59 @@ import data from './data.json'
 
 const options = {
   chart: {
-    id: "basic-bar"
+    toolbar: {
+      show: false
+    }
+  },
+  tooltip: {
+    fillSeriesColor: true,
+    theme: "dark",
+    y: {
+      formatter: function (val) {
+        return val.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+          useGrouping: true,
+          maximumFractionDigits: 0,
+          minimumFractionDigits: 0
+        });
+      }
+    }
   },
   xaxis: {
-    categories: data.dates
-  }
+    categories: data.dates,
+    tooltip: {
+      enabled: false
+    },
+    labels: {
+      formatter: function (timestamp) {
+        return new Date(timestamp).toDateString()
+      }
+    },
+    type: 'numeric',
+    tickAmount: 5,
+  },
+  yaxis: {
+    tickAmount: 4,
+  },
+  title: {
+    text: data.metrics[0].current,
+  },
 }
-
 const series = [
   {
-    name: "data",
-    data: data.metrics[0].values
+    name: "",
+    data: data.metrics[0].values,
   }
 ]
 
 export const LineChart = () => {
-  console.log(data.metrics)
-  console.log(data.dates)
   return (
     <div className="mixed-chart">
       <Chart
         options={options}
         series={series}
         type="line"
-      // width="500"
       />
     </div>
   );
